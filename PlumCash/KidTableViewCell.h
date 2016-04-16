6470ee3822ac27858064c019b0c9b95497d0d4ae
Cristina <ce@tsl.io>
2016-04-16 14:51:50 -0400

    added kidtableviewcell

---
+++ PlumCash/KidTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  KidTableViewCell.h
+//  PlumCash
+//
+//  Created by Cristina on 4/16/16.
+//  Copyright © 2016 SilverLogic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KidTableViewCell : UITableViewCell
+
+@end
---
+++ PlumCash/KidTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  KidTableViewCell.m
+//  PlumCash
+//
+//  Created by Cristina on 4/16/16.
+//  Copyright © 2016 SilverLogic. All rights reserved.
+//
+
+#import "KidTableViewCell.h"
+
+@implementation KidTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
